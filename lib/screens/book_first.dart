2414f46609535d2eae523daf3c0ba5d2ec9f4e10
Thilina929 <thilinamahesh929@gmail.com>
2025-02-05 00:57:08 +0530

    edit the book_first.dart file

--- lib/screens/book_first.dart
+++ lib/screens/book_first.dart
@@ -12,7 +12,7 @@ class BookFirstScreen extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Book(title: "MANDODARI", author: "MOHAN RAJH MADAWALA", price: "\$29.99", imageUrl: "assets/images/Book_01.jpg"),
+            Book(title: "MANDODARI Thilina", author: "MOHAN RAJH MADAWALA", price: "\$29.99", imageUrl: "assets/images/Book_01.jpg"),
             Book(title: "LOVINA", author: "MOHAN RAJH MADAWALA", price: "\$34.99", imageUrl: "assets/images/Book_02.jpg"),
             Book(title: "CAMELLIA", author: "VIRAJINI THENNAKON", price: "\$39.99", imageUrl: "assets/images/Book_03.jpg"),
 
